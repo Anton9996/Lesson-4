2c425c6c4484166f3e404fc63fae5d89d48673db
Антон <korjant09996@gmail.com>
2022-12-01 22:42:45 +0400

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А
+//N * (N + 1) / 2 (нашел формулу в интернете)
+Console.WriteLine("введите целое положительное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summOfNumbers(int a)
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        sum += i; // sum = sum + i
+    }
+    return sum;
+}
+
+if (number <= 0)
+{
+    Console.WriteLine($"По-моему это -> {number}, не положительное число");
+}
+else
+{
+    int result = summOfNumbers(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+}
