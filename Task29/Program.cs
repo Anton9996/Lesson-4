3d333fd61f6868618968cb3b2fc6c3917c3cd62a
Антон <korjant09996@gmail.com>
2022-12-04 20:46:25 +0400

    task29

---
+++ Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, заполненный псеводслучайными числами и выводит их на экран
+Console.WriteLine("Алгорит создает масив из 8 элементов, заполеный псевдослучайными числами");
+
+void FullArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write($"[");
+    while (position < count)
+    {
+        Console.Write($"{col[position]}  ");
+        position++;
+    }
+    Console.Write($"]");
+}
+
+int[] array = new int[8];
+
+FullArray(array);
+PrintArray(array);
