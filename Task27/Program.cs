bd43cdc01a9a22400af4c22e56d25d2bf3804d79
Антон <korjant09996@gmail.com>
2022-12-04 18:48:03 +0400

    Taask27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+// 452 -> 11
+
+Console.WriteLine("Напишите число и програма посчитает суммы цифр в этом числе");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countSumm(int num)
+{
+    int summ = 0;
+    while (num > 0)
+    {
+        summ = summ + num % 10;
+        num = num / 10;
+    }
+    return summ;
+}
+int result = countSumm(number);
+Console.WriteLine($"сумма цифр, числа {number} = {result}");
